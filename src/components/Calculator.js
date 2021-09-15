da75a84bf4ef13d77409454e9b1ce9d5fbdbcf15
Elyor Doniyorov <doniyorovelyor@gmail.com>
2021-09-15 13:28:38 +0500

    Add Calculator class

---
+++ src/components/Calculator.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Calculator.css';
+
+// eslint-disable-next-line react/prefer-stateless-function
+class Calculator extends React.Component {
+  render() {
+    return (
+      <div className="calc-container">
+        <div className="calc-display">0</div>
+        <button type="button" className="gray-btn">AC</button>
+        <button type="button" className="gray-btn">+/-</button>
+        <button type="button" className="gray-btn">%</button>
+        <button type="button" className="orange-btn">/</button>
+        <button type="button" className="gray-btn">7</button>
+        <button type="button" className="gray-btn">8</button>
+        <button type="button" className="gray-btn">9</button>
+        <button type="button" className="orange-btn">*</button>
+        <button type="button" className="gray-btn">4</button>
+        <button type="button" className="gray-btn">5</button>
+        <button type="button" className="gray-btn">6</button>
+        <button type="button" className="orange-btn">-</button>
+        <button type="button" className="gray-btn">1</button>
+        <button type="button" className="gray-btn">2</button>
+        <button type="button" className="gray-btn">3</button>
+        <button type="button" className="orange-btn">+</button>
+        <button type="button" className="gray-btn zero-btn">0</button>
+        <button type="button" className="gray-btn">.</button>
+        <button type="button" className="orange-btn">=</button>
+      </div>
+    );
+  }
+}
+
+export default Calculator;
