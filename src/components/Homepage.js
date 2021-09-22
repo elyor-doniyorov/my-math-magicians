d4ee32978c6780f26930c7d37c06299cc9954a20
elyor doniyorov <doniyorovelyor@gmail.com>
2021-09-22 12:57:51 +0500

    Update text of Homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -4,7 +4,7 @@ import './Homepage.css';
 function Homepage() {
   return (
     <div className="home">
-      <h2>Welcome on our Calculator</h2>
+      <h2>Welcome to our Calculator</h2>
       <p>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit.
         Numquam nobis id, provident reprehenderit,
