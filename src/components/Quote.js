6fcf94e0af09c6d0b5d30ab065248f0729bd7b0d
elyor doniyorov <doniyorovelyor@gmail.com>
2021-09-22 11:38:46 +0500

    Add structure and style to Quote

---
+++ src/components/Quote.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Quote.css';
+
+function Quote() {
+  return (
+    <div className="quote">
+      <p>
+        Mathematics is not about numbers, equations, computations,
+        or algorithms: it is about understanding. -William Paul Thurston
+      </p>
+    </div>
+  );
+}
+
+export default Quote;
